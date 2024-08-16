dcd09ada03e70e7ee953fac4382dbd046593666b
Liubov-Pysana <144674433+Liubov-Pysana@users.noreply.github.com>
2024-08-16 15:36:49 +0100

    TS hw 1

---
+++ src/basic/3.ts
@@ -0,0 +1,14 @@
+let mixedType: string | number ;
+
+mixedType = 'string'; 
+mixedType = 10; 
+
+export {};
+
+
+type OneOrTwo = 'enable' | 'disable' ;
+let value: OneOrTwo;
+value = 'enable'; 
+value = 'disable'; 
+
+export {};
